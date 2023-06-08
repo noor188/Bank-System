044aea0b5244debb7c623740aca68dafaea5bd89
Noor <noormmlk@yahoo.com>
2023-06-07 19:18:53 -0600

    Create Bank_system.cpp
    creating bank,account classes and main

---
+++ Banking_system.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+Class Account
+{
+	int accountNumber;
+	string firstName;
+	string lastName;
+	float balance;
+};
+
+CLass Bank
+{
+}
+
+int main()
+{
+	Bank bank;
+}
