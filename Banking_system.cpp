607c393a708d771930c4d80064b8c64ec403130b
Noor <noormmlk@yahoo.com>
2023-06-10 17:08:34 -0600

    Updated Bank_system.cpp
    created c++ script that prompts to the cmd prompt

--- Banking_system.cpp
+++ Banking_system.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< Updated upstream:Banking_system.cpp
-Class Account
-{
-	int accountNumber;
-	string firstName;
-	string lastName;
-	float balance;
-};
 
-CLass Bank
-{
-}
-
-=======
->>>>>>> Stashed changes:Bank-System.cpp
 int main()
 {
 	string Name;
