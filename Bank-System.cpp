fb9f19496ac6b1b3f34a5e3ee061df72708e0b89
Noor <noormmlk@yahoo.com>
2023-06-10 17:05:16 -0600

    Updating banking_system.cpp
    created c++ script #4

---
+++ Bank-System.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string Name;
+	cout << "*** Welcome to our Banking System ***" << endl;
+	cout << "Please enter your name ___";
+	cin >> Name;
+	cout << "Welcome " << Name << endl;
+}
