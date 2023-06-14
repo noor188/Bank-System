524d2481db4c677981ad122b42f2ae6a19d0fa6b
Noor <noormmlk@yahoo.com>
2023-06-13 18:57:06 -0600

    create classes properties and functions
    solved #12
    solved #13
    solved #14
    solved #15
    solved #16
    solved #17

--- Banking_system.cpp
+++ Banking_system.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 class Account
 {
+private:
+	long accountNumber;
+	string firstName;
+	string lastName;
+	float balance;
+
+public:
+	Account();
+	Account(string firtsName, string lastName, float balance);
+	void Deposit(float amount);
+	void Withdraw(float amount);
 };
 
 class Bank
 {
+private:
+	map<long, Account> accounts;
+
+public:
+	Bank();
+	Account openAccount(string firstName, string lastName, float balance);
+	void closeAccount(long accountNumber);
+	void Deposit(long accountNumber, float amount);
+	void Withdraw(long accountNumber, float amount);
+	Account balanceEnquiry(long accountNumber);
 };
 
 int main()
 {
-	string Name;
+	string fName;
 	cout << "*** Welcome to our Banking System ***" << endl;
-	cout << "Please enter your name ___";
-	cin >> Name;
-	cout << "Welcome " << Name << endl;
+	cout << "Please enter your first name ___";
+	cin >> fName;
+	cout << "Welcome " << fName << endl;
 }
